9168495f2f5534efd181ffa4ae52ee12dfafcccd
Zahid Hasan Emran <zahidhasanemran@gmail.com>
2024-09-12 21:30:40 +0600

    incoming call on console

--- index.js
+++ index.js
@@ -13,8 +13,17 @@ io.on("connection", socket => {
     const {email, room} = data;
     emailToSocketIdMap.set(email, socket?.id);
     socketIdToEmailMap.set(socket.id, email);
+
     io.to(room).emit("user:joined", {email, id: socket?.id})
     socket.join(room)
     io.to(socket?.id).emit("room:join", data)
   });
+
+
+  socket.on("user:call", ({to, offer}) => {
+    io.to(to).emit('incomming:call', {from: socket?.id, offer})
+  })
+
+
+
 })
